c4f5055fd4dc097e00e98a6ac5652eb64d68e1cb
Jonathan Berry <jberry@wpi.edu>
2018-11-14 18:36:45 -0500

    wip pre-debugging astar code

--- scripts/A_Star.py
+++ scripts/A_Star.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 import rospy
 from map_helper import *
+from nav_msgs import Path, PoseStamped
 from math import sqrt
+import PriorityQueue
 
 
 class A_Star:
 
     def __init__(self):
-
         """
-            This node handle A star paths requests.
+            This node handles A* path requests
             It is accessed using a service call. It can the publish grid cells
             to show the frontier, closed and path.
         """
@@ -31,7 +32,9 @@ class A_Star:
         self.wavefrontPublisher = rospy.Publisher('frontier', GridCells, queue_size=10)  # I don't actually know what this should be
 
         # for setting the path on rviz
-        self.pathPublisher = rospy.Publisher('path', GridCells, queue_size=1)
+        self.pathPublisher = rospy.Publisher('path', Path, queue_size=1)
+
+        start = (0,0) # variable to store the start cell todo: remove after testing
 
     def handle_a_star(self, req):
 
@@ -55,15 +58,51 @@ class A_Star:
 
     def a_star(self, start, goal):
         """
-            A*
-            This is where the A* algorithum belongs
+            The A* algorithm
             :param start: tuple of start pose
             :param goal: tuple of goal pose
-            :return: dict of tuples
+            :return: list of tuples along the optimal path
         """
-        pass
 
-      
+        frontier = PriorityQueue()
+        came_from = {}
+        cost_so_far = {}
+
+        # add the start cell
+        frontier.put(start, 0)
+        cost_so_far[start] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == goal:
+                break
+
+            for next in get_neighbors(current):
+
+                # find the total cost of going from start to next
+                cost = cost_so_far[current] + move_cost(current, next)
+
+                # if this is the first time exploring next, add it in
+                # or, if we just found a faster way to get to next, update it
+                if next not in cost_so_far or cost < cost_so_far[next]:
+                    cost_so_far[next] = cost
+                    priority = cost + euclidean_heuristic(next, goal)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+
+        # create the path by following came_from from goal back to the start
+        path = [goal]
+        current = goal
+        while current is not start:
+            current = came_from[current]
+            path.append(current)
+
+        path.reverse() # reverse the path to put it in the right order
+
+        return path
+
+
     def euclidean_heuristic(self, point1, point2):
         """
             calculate the dist between two points
@@ -81,26 +120,33 @@ class A_Star:
 
         return dist
 
-    def move_cost(self, current, next):
+    def move_cost(self, point1, point2):
         """
-              calculate the dist between two points
-              :param current: tuple of location
-              :param next: tuple of location
-              :return: dist between two points
+            calculate the Manhattan distance between two points
+            :param current: tuple of location
+            :param next: tuple of location
+            :return: Manhattan distance between two points
         """
-        pass
+        x1, y1 = point1
+        x2, y2 = point2
+
+        xdelta = abs(x2 - x1)
+        ydelta = abs(y2 - y1)
+
+        dist = xdelta + ydelta
+        return dist
 
 
-    def reconstruct_path(self, start, goal, came_from):
-        """
-            Rebuild the path from a dictionary
-            :param start: starting key
-            :param goal: starting value
-            :param came_from: dictionary of tuples
-            :return: list of tuples
-       """
-        pass
-  
+#    def reconstruct_path(self, start, goal, came_from):
+#        """
+#            Rebuild the path from a dictionary
+#            :param start: starting key
+#            :param goal: starting value
+#            :param came_from: dictionary of tuples
+#            :return: list of tuples
+#        """
+#        pass
+
 
     def optimize_path(self, path):
         """
@@ -122,11 +168,29 @@ class A_Star:
 
     def publish_path(self, points):
         """
-            Create a Path() and publishes the cells if Paint is true
+            Publishes a Path() containing the waypoints along the path
             :param points: list of tuples of the path
-            :return: Path()
+            :return:
         """
-        self.pathPublisher.publish(points)  # pass in dummy data for now
+        path = Path()
+
+        # copy all the points as PoseStamped() into the path
+        for point in points:
+
+            # get the real-world coordinates of the point relative to the
+            # origin of the map, from the tuple of grid coordinates A* returns
+            world_x, world_y = map_to_world(point[0], point[1], self.my_map)
+
+            # make a PoseStamped at the real-world coorinates of the point
+            # currenlty has the orientation set to the default, to be ignored
+            pose = PoseStamped()
+            pose.pose.position.x = world_x
+            pose.pose.position.y = world_y
+
+            # append the new pose to the list of poses that make up the path
+            path.poses.append(pose)
+
+        self.pathPublisher.publish(path)
 
     def display_start(self, msg):
         """
@@ -158,6 +222,7 @@ class A_Star:
         initcell.cell_height = self.my_map.info.resolution
         initcell.cells = points
 
+        self.start = newloc # todo: remove this after testing
         self.initPublisher.publish(initcell)
 
     def display_goal(self, msg):
@@ -192,13 +257,17 @@ class A_Star:
 
         self.goalPublisher.publish(goalcell)
 
+        # todo: remove this section after testing:
+        goal = newloc
+        self.a_star(start, goal)
+
 
 if __name__ == '__main__':
     alg = A_Star()
     print("made algorithm")
     rospy.sleep(1)
-    point1 = (0, 0)
-    point2 = (-3, 4)
-    print(alg.euclidean_heuristic(point1, point2))
+    #point1 = (0, 0)
+    #point2 = (-3, 4)
+    #print(alg.euclidean_heuristic(point1, point2))
     rospy.spin()
     pass
