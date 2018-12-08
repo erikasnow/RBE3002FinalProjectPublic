7a28501e727d28f74f93bc44250c24d91404d312
Erika <essnow@wpi.edu>
2018-12-08 14:43:52 -0500

    added other callbacks

--- scripts/Planning.py
+++ scripts/Planning.py
@@ -23,7 +23,7 @@ class Planning:
         # subscribes to astar path, scan map, and robot done message
         # is this actually how we need to subscribe to the astar service?
         self.pathSubscriber = rospy.Subscriber('path', Path, self.handle_path)  # TODO write callback
-        self.mapSubscriber = rospy.Subscriber('newmap', OccupancyGrid, self.handle_map)  # TODO write callback
+        self.mapSubscriber = rospy.Subscriber('newmap', OccupancyGrid, self.handle_map)
         # wait a sec... what if we had Robot publish the Pose it just finished moving to? That way we can double-check
         # and only send the next one when the message updates
         self.doneSubscriber = rospy.Subscriber('done', PoseStamped, self.handle_done)
@@ -38,6 +38,7 @@ class Planning:
     def handle_done(self, msg):
         """
         Publishes the next PoseStamped, given the current map and path to goal
+        :param msg: PoseStamped
         :return: void
         """
         # I'm worried this could potentially write over a new path given by astar
@@ -48,6 +49,24 @@ class Planning:
 
         # publish the next pose in the path
         self.wayptPublisher.publish(self.currpath.poses[0])
+
+    def handle_map(self, msg):
+        """
+        Updates the map used for astar
+        :param msg: OccupancyGrid
+        :return:
+        """
+        # where do we call astar from here? main?
+        self.currmap = msg
+
+    # I think this is probably wrong, but I don't know how to do it for a service
+    def handle_path(self, msg):
+        """
+        Updates the path we get from astar
+        :param msg: Path
+        :return:
+        """
+        self.currpath = msg
 
 
 if __name__ == '__main__':
